848c8ed7a3a57b91255c26922aba7cf5f3220835
ManiTG <71017734+ManiTG@users.noreply.github.com>
2020-11-14 16:30:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var canvas;
+//create a preload function and load all the images
+//use the image using image function wherever needed
+// p5 reference 
+function setup(){
+canvas=createCanvas(displayWidth,displayHeight)
+form= new Form();
+
+}
+
+function draw(){
+form.display(); 
+background("red");
+}
+
